2386c6c380b5348af6513a264b8d4c6ebd37d7f8
Elman <200522elmanok>
2024-08-10 17:15:52 +0500

    Сделал url контроллер

---
+++ src/controllers/urlController.ts
@@ -0,0 +1,36 @@
+import validateUrl from "../utils/validator";
+import { v4 as uuidv4 } from "uuid";
+import { Request, Response } from "express";
+
+interface UrlDatabase {
+  [shortcode: string]: string;
+}
+
+const urlDatabase: UrlDatabase = {};
+
+export const shortenUrl = (req: Request, res: Response) => {
+  const { url } = req.body;
+
+  if (!validateUrl(url)) {
+    return res.status(400).json({ error: "Invalid URL" });
+  }
+
+  const shortcode = uuidv4();
+  urlDatabase[shortcode] = url;
+
+  res.status(200).json({
+    shortcode,
+    redirect: `${req.protocol}://${req.get("host")}/${shortcode}`,
+  });
+};
+
+export const redirectUrl = (req: Request, res: Response) => {
+  const { shortcode } = req.params;
+  const originalUrl = urlDatabase[shortcode];
+
+  if (originalUrl) {
+    return res.redirect(302, originalUrl);
+  } else {
+    return res.status(404).json({ error: "Not found" });
+  }
+};
